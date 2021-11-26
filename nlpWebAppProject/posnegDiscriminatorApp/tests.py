3894a656740ed4862e4307f0284a06ae768e5190
sekine-kiyo(WSL) <sekine.kiyo.1203@gmail.com>
2021-11-26 21:29:22 +0900

    URLディスパッチャのルーティングを加味したビューテストを定義

--- nlpWebAppProject/posnegDiscriminatorApp/tests.py
+++ nlpWebAppProject/posnegDiscriminatorApp/tests.py
@@ -4,6 +4,8 @@ from posnegDiscriminatorApp.views import top
 
 # Create your tests here.
 
+'''
+トップページのステータスコードとレスポンスをテストする単純なコード
 class TopPageViewTest(TestCase):
     def test_top_returns_200(self):
         request = HttpRequest()
@@ -14,3 +16,19 @@ class TopPageViewTest(TestCase):
         request = HttpRequest()
         response = top(request)
         self.assertEqual(response.content, b"Hello World!")
+'''
+# ↑のテストケースに、URLディスパッチャのルーティングが
+# 正しく動作しているかテストするのが以下
+class TopPageRoutingTest(TestCase):
+    def test_top_returns_200(self):
+        '''
+        Clientクラスのオブジェクトclientを用いることで、
+        任意のエンドポイントに対してGETやPOSTを送った際の
+        挙動が確認できる
+        '''
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_top_returns_expected_content(self):
+        response = self.client.get('/')
+        self.assertEqual(response.content, b'Hello World!')
